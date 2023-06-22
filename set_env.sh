4167766be3f6145ec99f7031d8c7ffbd2484fc63
Phuc Dep Trai <nhatphucpham@hotmail.com>
2023-06-22 23:02:07 +0700

    fix env

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.ccux0r3njccz.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRESS_USERNAME=postgres
+export POSTGRESS_PASSWORD=postgres
+export POSTGRESS_HOST=postgres.ccux0r3njccz.us-east-1.rds.amazonaws.com
+export POSTGRESS_DB=postgres
 export AWS_BUCKET=phucpnbucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
